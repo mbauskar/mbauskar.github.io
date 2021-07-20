c73700b375a572bc877d870713f43031299b6391
Makarand Bauskar <mbauskar@gmail.com>
2021-07-20 10:10:01 +0530

    added verify parser on the github pages

---
+++ verify-parser/precache-manifest.6c6b28efa837a2046c83c2450080a483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6ba294194dd2ab6627e38346e6a03f",
+    "url": "/verify-parsers/index.html"
+  },
+  {
+    "revision": "955492b52758f9877d61",
+    "url": "/verify-parsers/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a963d0d336d6977df33e",
+    "url": "/verify-parsers/static/css/main.b4edef91.chunk.css"
+  },
+  {
+    "revision": "955492b52758f9877d61",
+    "url": "/verify-parsers/static/js/2.89c56f37.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/verify-parsers/static/js/2.89c56f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a963d0d336d6977df33e",
+    "url": "/verify-parsers/static/js/main.e4589f60.chunk.js"
+  },
+  {
+    "revision": "5d3aef23a6f2fae67d80",
+    "url": "/verify-parsers/static/js/runtime-main.0cd7d813.js"
+  }
+]);
